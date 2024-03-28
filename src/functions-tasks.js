d66e136468fac347e905df0691ce74c765294b47
Dima Usov <dmitry.usov@yahoo.com>
2024-03-28 11:12:07 +0300

    feat: add getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -225,8 +225,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom - 1;
+  return function invoke() {
+    counter += 1;
+    return counter;
+  };
 }
 
 module.exports = {
